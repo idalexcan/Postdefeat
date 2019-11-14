1efa71f3963ae9d0071d4d9fec05a14c973aaae2
Alexis Cano <id.alexcan@gmail.com>
2019-11-14 10:22:39 -0500

    silvestre changón
    vivimos en una sociedad donde quiero tamales a toda hora

--- Assets/Scripts/CollManager.cs
+++ Assets/Scripts/CollManager.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public enum Agent
 {
     heroshipFocus,
+    EnemyA_Bullet
 }
 
 public class CollManager : MonoBehaviour
@@ -19,7 +20,12 @@ public class CollManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        switch (agent)
+        {
+            case Agent.EnemyA_Bullet:
+                BulletEnemA();
+                break;
+        }
     }
 
     void OnTriggerEnter(Collider other)
@@ -27,13 +33,35 @@ public class CollManager : MonoBehaviour
         switch (agent)
         {
             case Agent.heroshipFocus:
-                Destroy(other.gameObject);
+                if (other.GetComponent<Heroship>()==false)
+                {
+                    Destroy(other.gameObject);
+                }
+                break;
+            case Agent.EnemyA_Bullet:
+                Destroy(gameObject);
                 break;
             default:
                 break;
         }
     }
 
+    int timer=0;
+    void BulletEnemA()
+    {
+        timer++;
+        if (timer>5)
+        {
+            if (timer>70)
+            {
+                Destroy(gameObject);
+            }
+            else
+            {
+                transform.localScale+=new Vector3(1,1,1)*0.005f;
+            }
+        }
+    }
     
     
 }
---
+++ Assets/Scripts/EnemyA.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyA : MonoBehaviour
+{
+    public GameObject bulletPos, bullet;
+    Vector2 randomFollow;
+
+    int timer, timerLimit=15;
+    void Awake()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            randomFollow=new Vector2(Random.Range(-1f,1f), Random.Range(-1f,1f));
+            transform.position=new Vector3(randomFollow.x, randomFollow.y, transform.position.z);
+        }
+
+        timer++;
+        if (timer==timerLimit)
+        {
+            GameObject droped=Instantiate(bullet);
+            droped.transform.position=bulletPos.transform.position;
+            droped.GetComponent<Rigidbody>().AddForce(transform.up*(-100));
+            droped.AddComponent<CollManager>().agent=Agent.EnemyA_Bullet;
+            timer=0;
+            timerLimit=Random.Range(10, 50);
+        }
+
+        transform.position+=transform.up*0.1f;
+    }
+}
